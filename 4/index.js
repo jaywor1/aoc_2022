91d1ebef308a7a438426c4382afe47194c3ca558
Michal Javor <javor.mi.2020@skola.ssps.cz>
2022-12-04 17:02:18 +0100

    day 4

---
+++ 4/index.js
@@ -0,0 +1,45 @@
+function solution1(input) {
+    const lines = input.split('\n');
+    let sol = 0;
+    for (let i = 0; i < lines.length - 1; i++) {
+        let items = lines[i].split(',');
+        let a = items[0].split('-');
+        let b = items[1].split('-');
+        if (Number(a[0]) <= Number(b[0]) && Number(a[1]) >= Number(b[1])) {
+            sol++;
+            continue;
+        }
+        else if (Number(a[0]) >= Number(b[0]) && Number(a[1]) <= Number(b[1])) {
+            sol++;
+            continue;
+        }
+    }
+    return sol;
+}
+
+function solution2(input) {
+    const lines = input.split('\n');
+    let sol = 0;
+    for (let i = 0; i < lines.length - 1; i++) {
+        let items = lines[i].split(',');
+        let a = items[0].split('-');
+        let b = items[1].split('-');
+        if (Number(a[1]) == Number(b[1])) {
+            sol++;
+            continue;
+        }
+        else if (Number(a[1]) > Number(b[1])) {
+            if (Number(b[1]) >= Number(a[0])) {
+                sol++;
+                continue;
+            }
+        } else if (Number(a[1]) < Number(b[1])) {
+            if (Number(a[1]) >= Number(b[0])) {
+                sol++;
+                continue;
+            }
+        }
+
+    }
+    return sol;
+}
