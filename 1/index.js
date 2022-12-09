3d12e63f1e4c696684a4e91e57193445dc0b2628
Michal Javor <javor.mi.2020@skola.ssps.cz>
2022-12-09 15:44:22 +0100

    formating changes

--- 1/index.js
+++ 1/index.js
@@ -1,35 +1,41 @@
-function solution1(input){
-    const arr = input.split('\n');
+const fs = require("fs");
+const aoc = require("aoc_testing");
+const input = fs.readFileSync("input.txt").toString();
 
+aoc.logSolution(input, solution1, solution2);
+aoc.checkTime(input, 1000, solution1, solution2);
+
+function solution1(input) {
+    const arr = aoc.crlfBullshit(input.split('\n'));
     let currentElfCallories = 0;
     let sol = 0;
 
-    for(let i = 0;i < arr.length;i++){
-        if(arr[i] == ''){
-            if(currentElfCallories > sol){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '') {
+            if (currentElfCallories > sol) {
                 sol = currentElfCallories;
             }
             currentElfCallories = 0;
         }
-        else{
+        else {
             currentElfCallories += Number(arr[i]);
         }
     }
     return sol;
 }
 
-function solution2(input){
-    const arr = input.split('\n');
+function solution2(input) {
+    const arr = aoc.crlfBullshit(input.split('\n'));
 
     let currentElfCallories = 0;
     let elfs = [];
 
-    for(let i = 0;i < arr.length;i++){
-        if(arr[i] == ''){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '') {
             elfs.push(currentElfCallories);
             currentElfCallories = 0;
         }
-        else{
+        else {
             currentElfCallories += Number(arr[i]);
         }
     }
@@ -37,7 +43,7 @@ function solution2(input){
     let sol = 0;
 
 
-    for(let i = 0;i < 3;i++){
+    for (let i = 0; i < 3; i++) {
         let biggestElf = Math.max(...elfs);
         sol += biggestElf;
         elfs.splice(elfs.indexOf(biggestElf));
