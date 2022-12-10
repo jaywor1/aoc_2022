93e37419223f18f58a6a2c5bccea96e8fbe198bf
Michal Javor <javor.mi.2020@skola.ssps.cz>
2022-12-10 23:29:38 +0100

    day 10 part 2

--- 10/index.js
+++ 10/index.js
@@ -3,7 +3,8 @@ const aoc = require("aoc_testing");
 
 const input = fs.readFileSync("input.txt").toString();
 
-console.log(solution1(input));
+aoc.logSolution(input, solution1, solution2);
+aoc.checkTime(input, solution1, solution2);
 
 function solution1(input) {
     const lines = input.split('\n');
@@ -38,4 +39,51 @@ function solution1(input) {
     }
 
     return sol;
+}
+
+function solution2(input) {
+    const lines = input.split('\n');
+
+    let sol = ["", "", "", "", "", ""];
+    let yAxis = 0;
+    let cycle = 0;
+    let register = 1;
+    for (let i = 0; i < lines.length; i++) {
+        let instruction = lines[i].split(' ');
+        if (instruction.length == 2) {
+            for (let y = 0; y < 2; y++) {
+                cycle++;
+                if (register - 1 < cycle && cycle < register + 3) {
+                    sol[yAxis] += '#';
+                }
+                else {
+                    sol[yAxis] += '.';
+                }
+                if (cycle % 40 == 0) {
+                    yAxis++;
+                    register += 40;
+                }
+            }
+            register += Number(instruction[1]);
+        }
+        else {
+            cycle++;
+            if (register - 1 < cycle && cycle < register + 3) {
+                sol[yAxis] += '#';
+            }
+            else {
+                sol[yAxis] += '.';
+            }
+            if (cycle % 40 == 0) {
+                yAxis++;
+                register += 40;
+            }
+        }
+
+
+
+    }
+
+    return sol;
+
 }
