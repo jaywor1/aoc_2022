e36fe168f87cb729502c08c7e42bef48c326192e
Michal Javor <javor.mi.2020@skola.ssps.cz>
2022-12-10 12:17:51 +0100

    day 9 part 2

--- 9/index.js
+++ 9/index.js
@@ -3,7 +3,8 @@ const aoc = require("aoc_testing");
 
 const input = fs.readFileSync("input.txt").toString();
 
-console.log(solution1(input));
+aoc.logSolution(input, solution1, solution2);
+aoc.checkTime(input, solution1, solution2);
 
 function isTouching(head, tail) {
     for (let x = head.x - 1; x < head.x + 2; x++) {
@@ -32,7 +33,6 @@ function uniqueMoves(moves) {
     }
     return uniqueMov.length;
 }
-
 
 function solution1(input) {
     const lines = aoc.crlfBullshit(input.split('\n'));
@@ -87,8 +87,68 @@ function solution1(input) {
         }
 
     }
-
     return uniqueMoves(moves);
+}
 
+function solution2(input) {
+    const lines = aoc.crlfBullshit(input.split('\n'));
+    let head = { x: 0, y: 0 };
+    let tails = [];
+    for (let i = 0; i < 9; i++) {
+        tails.push({ x: 0, y: 0 });
+    }
+    let moves = [{ x: 0, y: 0 }];
+    for (i in lines) {
+        let currentLine = lines[i].split(' ');
+        for (let y = 0; y < currentLine[1]; y++) {
+            switch (currentLine[0]) {
+                case 'U':
+                    head.y++;
+                    break;
+                case 'D':
+                    head.y--;
+                    break;
+                case 'R':
+                    head.x++;
+                    break;
+                case 'L':
+                    head.x--;
+                    break;
+            }
+            for (let z = 0; z < tails.length; z++) {
+                let lastRef = (z == 0) ? head : tails[z - 1];
+                if (!isTouching(lastRef, tails[z])) {
+                    if (lastRef.x == tails[z].x) {
+                        if (lastRef.y > tails[z].y)
+                            tails[z].y++;
+                        else
+                            tails[z].y--;
 
+                    }
+                    else if (lastRef.y == tails[z].y) {
+                        if (lastRef.x > tails[z].x)
+                            tails[z].x++;
+                        else
+                            tails[z].x--;
+
+                    }
+                    else {
+                        if (lastRef.y > tails[z].y)
+                            tails[z].y++;
+                        else
+                            tails[z].y--;
+                        if (lastRef.x > tails[z].x)
+                            tails[z].x++;
+                        else
+                            tails[z].x--;
+                    }
+                    if (z == 8)
+                        moves.push({ x: tails[z].x, y: tails[z].y });
+                }
+            }
+
+        }
+
+    }
+    return uniqueMoves(moves);
 }
